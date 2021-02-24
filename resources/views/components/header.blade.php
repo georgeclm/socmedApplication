5ed6a135b1fafa8ac995886d153bed392ead8f12
George <cavidjaja@gmail.com>
2021-02-24 10:17:21 +0700

    refine

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -13,7 +13,7 @@ $dmImage = ProfilesController::dmImage();
         <div class="container-fluid">
             <a class="navbar-brand" href="/">
                 <img src="{{ asset('img/logoicon.ico') }}" alt="" width="33" height="33"
-                    class="d-inline-block align-top"> SocMedApp</a>
+                    class="d-inline-block align-top"> SocMed</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Tsoggle navigation">
